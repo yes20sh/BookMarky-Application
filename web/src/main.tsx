9f527a8a4fad17c7b8fd16dcaa09fbf16b7562ea
Yashraj Singh Solanki <yes20sh@gmail.com>
2025-07-17 12:38:17 +0000

    feat (Yash) -> Remove the  ReactQueryDevtools

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,3 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,7 +5,6 @@ import './styles/index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>
